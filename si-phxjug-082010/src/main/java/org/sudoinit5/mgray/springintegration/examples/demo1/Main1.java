279906f094b82e9bf39f7c48961ec7c7e69462b6
Mario Gray <mario@gmail.com>
2010-09-08 17:04:01 -0700

    Moved into git before JUG. Unbreak code!

--- si-phxjug-082010/src/main/java/org/sudoinit5/mgray/springintegration/examples/demo1/Main1.java
+++ si-phxjug-082010/src/main/java/org/sudoinit5/mgray/springintegration/examples/demo1/Main1.java
@@ -3,7 +3,7 @@ package org.sudoinit5.mgray.springintegration.examples.demo1;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("demo1.xml");
 
